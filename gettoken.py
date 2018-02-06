c8a996add77331f97102078252c3174a33cadd9a
Alexandre Santos <alexandre.santos@ge.com>
2018-02-06 10:24:33 -0800

    settings moved to PWD/..

--- gettoken.py
+++ gettoken.py
@@ -1,15 +1,26 @@
-import sys
-
-target=sys.argv[1]
 
 a = "{{protocol}}://{{uaa-host}}/oauth/token"
 
 
-filepath = f'./{target}/settings'  
+# load settings
+
+kv = {}
+# the "target" folder stores the current folder storing the settings
+# the folder is mapped via docker volumes
+filepath = f'./target/settings'  
 with open(filepath) as fp:  
    line = fp.readline()
    while line:
        data = line.rstrip('\n').split(":",2)
-       a = a.replace("{{" + data[0] + "}}", data[1])
+       kv[data[0]] = data[1]
        line = fp.readline()
+
+for k in kv:
+    a = a.replace("{{" + k + "}}", kv[k])
+
+print(a)
+
+text_file = open("./target/Output.txt", "w")
+text_file.write("a" + a)
+text_file.close()
 
